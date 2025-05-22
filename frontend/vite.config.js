5266db829f18a5b523754e6d67d98c18cc8c964a
sachinfer <sachinnavoda74@gmail.com>
2025-05-22 21:16:48 +0530

    .

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
-=======
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -17,5 +9,4 @@ export default defineConfig({
       '/api': 'http://localhost:5000',
     },
   },
-}); 
->>>>>>> baf8337 (inint changes)
+})
